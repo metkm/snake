16af499da039319b00db8115ebd238bcfc2e884d
metkm <metinkorkmaz417@gmail.com>
2023-07-31 12:25:33 +0300

    select playlists

--- src/components/SpotifyPlaylists.vue
+++ src/components/SpotifyPlaylists.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { useProfileStore } from "../store";
 import { storeToRefs } from "pinia";
-import { Playlists } from "../models/Playlists";
-
+import { Playlists, SimplifiedPlaylist } from "../models/Playlists";
 import { ref } from "vue";
 
 const profileStore = useProfileStore();
@@ -13,22 +12,36 @@ const response = await fetch("https://api.spotify.com/v1/me/playlists", {
 });
 
 const playlists = ref<Playlists>(await response.json());
+const selectedPlaylists = ref<SimplifiedPlaylist[]>([]);
+
+const isSelected = (list: SimplifiedPlaylist) => {
+  return selectedPlaylists.value.includes(list);
+};
 </script>
 
 <template>
-  <div class="grid bg-slate-900 text-white rounded-lg overflow-hidden shadow border border-slate-600">
+  <div
+    class="grid bg-slate-900 text-white rounded-lg divide-y divide-slate-800 overflow-hidden shadow border border-slate-600"
+  >
     <div
       v-for="list in playlists.items"
       :key="list.id"
-      class="flex items-center p-2 gap-2 hover:bg-slate-800"
+      class="flex items-center p-2 gap-2 hover:bg-slate-800 relative transition-colors"
+      :class="{ 'bg-emerald-800 hover:bg-emerald-700': isSelected(list) }"
     >
+      <input
+        v-model="selectedPlaylists"
+        :value="list"
+        type="checkbox"
+        class="absolute inset-0 appearance-none"
+      />
       <img
         :src="list.images[0].url"
         width="40"
         height="40"
         class="aspect-square object-cover rounded"
       />
-      <p class="text-sm">{{ list.name }}</p>
+      <p class="text-sm select-none">{{ list.name }}</p>
     </div>
   </div>
 </template>
