afca99983f6087e0aa4c455d9d27023a0aac72e2
metkm <metinkorkmaz417@gmail.com>
2023-08-01 14:31:44 +0300

    update design of playlists

--- src/components/SpotifyPlaylists.vue
+++ src/components/SpotifyPlaylists.vue
@@ -30,58 +30,30 @@ const onChange = async (id: string, name: string) => {
 </script>
 
 <template>
-  <div>
-    <ul class="grid bg-slate-900 text-white rounded-lg overflow-hidden">
-      <li
-        v-for="list in playlists"
-        :key="list.id"
-        class="flex justify-between p-2 hover:bg-slate-800 gap-4"
-      >
-        <div class="flex gap-2 items-center">
-          <img
-            :src="list.images[0].url"
-            width="40"
-            height="40"
-            class="rounded object-cover aspect-square"
-          />
-          <label :for="list.id" class="text-sm">{{ list.name }}</label>
-        </div>
-
-        <input
-          v-model="selectedPlaylists"
-          @change="onChange(list.id, list.name)"
-          :value="list.id"
-          :id="list.id"
-          type="checkbox"
-        />
-      </li>
-    </ul>
-  </div>
-
-  <!-- <div
-    class="grid text-white rounded-lg overflow-hidden shadow transition-colors"
-  >
-    <div
-      v-for="list in playlists.items"
+  <ul class="grid bg-slate-900 text-white rounded-lg overflow-hidden">
+    <li
+      v-for="list in playlists"
       :key="list.id"
-      class="flex items-center p-2 gap-2 hover:bg-slate-800 relative transition-colors"
-      :class="{
-        'bg-emerald-800 hover:bg-emerald-700': playlistStore.isSelected(list),
-      }"
+      class="flex items-center justify-between p-2 hover:bg-slate-800 gap-4"
     >
+      <div class="flex gap-2 items-center">
+        <img
+          :src="list.images[0].url"
+          width="40"
+          height="40"
+          class="rounded object-cover aspect-square"
+        />
+        <label :for="list.id" class="text-sm">{{ list.name }}</label>
+      </div>
+
       <input
-        v-model="playlistStore.selectedPlaylists"
-        :value="list"
+        v-model="selectedPlaylists"
+        @change="onChange(list.id, list.name)"
+        :value="list.id"
+        :id="list.id"
         type="checkbox"
-        class="absolute inset-0 appearance-none"
+        class="appearance-none w-4 h-4 bg-slate-700 rounded checked:bg-slate-500 checked:bg-[url('/tick.svg')]"
       />
-      <img
-        :src="list.images[0].url"
-        width="40"
-        height="40"
-        class="aspect-square object-cover rounded"
-      />
-      <p class="text-sm select-none">{{ list.name }}</p>
-    </div>
-  </div> -->
+    </li>
+  </ul>
 </template>
