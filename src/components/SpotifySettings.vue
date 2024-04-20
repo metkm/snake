6a4b4db27041842c86c4b55209ba7b1a47631d39
metkm <metinkorkmaz417@gmail.com>
2024-04-20 16:06:16 +0300

    update label with the count

--- src/components/SpotifySettings.vue
+++ src/components/SpotifySettings.vue
@@ -6,11 +6,11 @@ const settingsStore = useSettingsStore();
 </script>
 
 <template>
-  <div class="bg-neutral-900/40 p-2 rounded-lg">
+  <div class="p-2 rounded-lg bg-neutral-900/40">
     <h1 class="mb-2 text-xs">Settings</h1>
 
     <div class="flex flex-col">
-      <label for="changeLimit">Change song every X points</label>
+      <label for="changeLimit">Change song every {{ settingsStore.songChangeLimit }} points</label>
       <BaseInput id="changeLimit" type="number" v-model="settingsStore.songChangeLimit" />
     </div>
   </div>
